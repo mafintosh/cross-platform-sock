04e240712fd0c4574483db329c8e81379f3fad89
Mathias Buus <mathiasbuus@gmail.com>
2018-03-06 17:11:06 +0100

    refactor

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const path =  require('path')
+const path = require('path')
+const fs = require('fs')
+
+const isWindows = process.platform === 'win32'
 
 xsock.unlink = unlink
 module.exports = xsock
 
 function unlink (name, cb) {
   if (!cb) cb = noop
-  if (process.platform === 'win32') process.nextTick(cb)
-  else require('fs').unlink(name, cb)
+  if (isWindows) process.nextTick(cb)
+  else fs.unlink(name, cb)
 }
 
 function xsock (name) {
-  return process.platform === 'win32'
+  return isWindows
     ? '\\\\.\\pipe\\xsock\\' + path.resolve(name)
     : path.resolve(name)
 }
